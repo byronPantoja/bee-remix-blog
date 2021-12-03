d2c759a01fcbf3d86fd087e318a7884aeb073fb3
dbp <byronpantoja@gmail.com>
2021-12-04 05:05:59 +0800

    adding posts route

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,24 +1,23 @@
-import { useLoaderData } from "remix";
+import { Link, useLoaderData } from "remix";
+import { getPosts } from "~/post";
+import type { Post } from "~/post";
 
 export const loader = () => {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+  return getPosts();
 };
 
 export default function Posts() {
-  const posts = useLoaderData();
-  console.log(posts);
+  const posts = useLoaderData<Post[]>();
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
